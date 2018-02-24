7d29060101cc3be8a174251cf9410b551b5f794e
nelsona11 <nelsona2523@gmail.com>
2018-02-24 16:54:43 -0500

    added a barracks class

---
+++ src/bot/discord/game/buildings/Barracks.java
@@ -0,0 +1,10 @@
+package bot.discord.game.buildings;
+
+public class Barracks extends Building{
+
+  public Barracks(){
+    //change ints later
+    super("barracks", 25, 2);
+  }
+
+}
--- src/bot/discord/game/buildings/Building.java
+++ src/bot/discord/game/buildings/Building.java
@@ -1,10 +1,17 @@
+package bot.discord.game.buildings;
 
 public abstract class Building {
+  //name of the building
   private String name;
+  //its current level
   private int level;
+  //max level it can be
   private int cap;
+  //current experience
   private int experience;
+  //total experience needed to level up
   private int expNeeded;
+  //how much expNeeded scales by per level
   private int expPerLevel;
 
   Building(){
@@ -21,10 +28,12 @@ public abstract class Building {
     this.level = 0;
     this.cap = newCap;
     this.experience = 0;
-    this.expNeeded = 0;
+    this.expNeeded = 100;
     this.expPerLevel = expPerLevel;
   }
 
+//adds experience to the Building
+//increases its level if experience is greater than or equal to expNeeded
   public void upgrade(int experience){
     this.experience += experience;
     if (this.experience >= this.expNeeded){
